daa1fae57fbfef20cf735e4d3d446350e6799d75
SunM1sty <mis7akes@mail.ru>
2023-06-11 22:32:59 +0300

    refactor: added config rules for linters

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,32 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true,
+    node: true
   },
-  extends: [
-    '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended',
-    'prettier',
-  ],
+  extends: ['@nuxtjs/eslint-config-typescript', 'plugin:nuxt/recommended', 'prettier'],
   plugins: [],
   // add your custom rules here
-  rules: {},
+  rules: {
+    'no-plusplus': 'off',
+    'no-underscore-dangle': 'off',
+    'no-restricted-syntax': 'off',
+    'class-methods-use-this': 'off',
+    'default-case': 'off',
+    'array-callback-return': 'off',
+    'consistent-return': 'off',
+    'no-console': [
+      2,
+      {
+        allow: ['info', 'warn', 'error']
+      }
+    ],
+    'max-len': [
+      'error',
+      {
+        code: 100,
+        ignoreUrls: true,
+        ignoreRegExpLiterals: true
+      }
+    ]
+  }
 }
