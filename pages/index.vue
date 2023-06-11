ba6c6b5cb68719fd2c09194886650ed36d9a2a09
SunM1sty <mis7akes@mail.ru>
2023-06-11 16:54:49 +0300

    fix: return removed PR template && add examples of vue components

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,16 @@
 <template>
-  <p>Hello world</p>
+  <HelloWorld />
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
+import HelloWorld from '~/components/HelloWorld.vue';
+
 export default Vue.extend({
   name: 'IndexPage',
+  components: {
+    HelloWorld
+  }
 })
 </script>
