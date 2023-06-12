a1afb81ae20dc385f0fd9354fdde6adc766f4009
SunM1sty <mis7akes@mail.ru>
2023-06-12 18:23:22 +0300

    feat(PR): add test component for check PR template

---
+++ components/TestComponent/TestComponent.stories.ts
@@ -0,0 +1,5 @@
+export default {
+  title: 'TestComponent'
+}
+
+export const TestComponent = () => '<TestComponent />'
---
+++ components/TestComponent/TestComponent.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1 class="header-title">Hello world</h1>
+</template>
+
+<style lang="scss" scoped>
+.header-title {
+  @include h1-font;
+}
+</style>
+
