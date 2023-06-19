e39d8ae12cd423b3c5cd05b46d6372906a8011b5
SunM1sty <mis7akes@mail.ru>
2023-06-19 04:09:08 +0300

    delete: remove trash files and useless lines of code

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,6 +34,10 @@ export default {
     }
   },
 
+  storybook: {
+    addons: ['@storybook/addon-controls', '@storybook/addon-docs']
+  },
+
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
