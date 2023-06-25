fb38bdf0c9920907d1357244fc75c611baf47530
SunM1sty <mis7akes@mail.ru>
2023-06-26 00:37:37 +0300

    hotfix: delete redirecting on last possible page, because of blocking main thread

--- middleware/redirect.js
+++ middleware/redirect.js
@@ -1,16 +1,7 @@
-export default function ({ redirect, route, store }) {
+export default function ({ redirect, route }) {
   const openingPage = Number(route.params.page)
 
   if (!openingPage || openingPage < 1) {
     return redirect('/1', route.query)
-  }
-
-  const pagination = store.getters.getNewsPagination
-  const paginationItemKeys = Object.keys(pagination)
-  const lastKey = paginationItemKeys[paginationItemKeys.length - 1]
-  const lastNewsPage = pagination[lastKey]
-
-  if (openingPage > lastNewsPage) {
-    return redirect(`/${lastNewsPage}`, route.query)
   }
 }
